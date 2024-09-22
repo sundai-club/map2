ff5624d65bd91599b54fae153b19c573854b443a
Anushka Singh <31279432+anushkasingh98@users.noreply.github.com>
2024-09-22 17:35:58 -0400

    converting code to functions that can be called

--- umap_impl.py
+++ umap_impl.py
@@ -13,31 +13,39 @@ def convert_to_embeddings(texts):
 
     return embeddings
 
-# Sample data: Replace this with your actual data loading
-# Load data from CSV file
-data = pd.read_csv('/Users/anushkasingh/Desktop/Code/hobby-projects/map2/2023-02-27-yc-companies.csv')
-df = pd.DataFrame(data)
 
-print(df.head())
+def load_and_save_embeddings(file_path, save_file_name):
+    # Load data from CSV file
+    data = pd.read_csv(file_path)
+    df = pd.DataFrame(data)
 
-texts = [" Company Name: " + str(df['Company'][i]) + " Description: " + str(df['Description'][i]) + " Sector: " + str(df['Sector'][i]) + " Location: " + str(df['Location'][i]) for i in range(len(df))]
-print(texts)
-print("\n\n")
-embeddings = convert_to_embeddings(texts)
+    print(df.head())
 
-# Initialize UMAP model
-reducer = umap.UMAP(n_components=2, random_state=42)
+    texts = [" Company Name: " + str(df['Company'][i]) + " Description: " + str(df['Description'][i]) + " Sector: " + str(df['Sector'][i]) + " Location: " + str(df['Location'][i]) for i in range(len(df))]
+    print(texts)
+    print("\n\n")
+    embeddings = convert_to_embeddings(texts)
 
-# Fit and transform the data
-f2d_embedding = reducer.fit_transform(embeddings)
+    # Initialize UMAP model
+    reducer = umap.UMAP(n_components=2, random_state=42)
 
-print("Embeddings shape:", f2d_embedding.shape)
+    # Fit and transform the data
+    f2d_embedding = reducer.fit_transform(embeddings)
 
-# Plot the 2D embeddings
-plt.figure(figsize=(10, 8))
-plt.scatter(f2d_embedding[:, 0], f2d_embedding[:, 1], s=5, cmap='Spectral')
-plt.title('UMAP projection of the embeddings')
-plt.xlabel('UMAP 1')
-plt.ylabel('UMAP 2')
-plt.colorbar()
-plt.show()
+    print("\n\nEmbeddings shape:", f2d_embedding.shape)
+    print("\n\nEmbedding Type:", type(f2d_embedding))
+
+    np.save(save_file_name, f2d_embedding)
+    # f2d_embedding = np.load('f2d_embedding.npy')
+
+# # Plot the 2D embeddings
+# plt.figure(figsize=(10, 8))
+# plt.scatter(f2d_embedding[:, 0], f2d_embedding[:, 1], s=5, cmap='Spectral')
+# plt.title('UMAP projection of the embeddings')
+# plt.xlabel('UMAP 1')
+# plt.ylabel('UMAP 2')
+# plt.colorbar()
+# plt.show()
+
+if __name__ == '__main__':
+    load_and_save_embeddings('/Users/anushkasingh/Desktop/Code/hobby-projects/map2/2023-02-27-yc-companies.csv', 'yc_f2d_embedding.npy')
